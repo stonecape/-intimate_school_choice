a50a79e0ba389ea73a707c22f436dfe542db8077
stonecape <capechy@hotmail.com>
2016-04-02 19:57:24 +0800

    初始提交

---
+++ intimate/src/main/java/com/bistu/intimate/App.java
@@ -0,0 +1,13 @@
+package com.bistu.intimate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
